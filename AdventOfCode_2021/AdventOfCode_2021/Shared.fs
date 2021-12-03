b1d07a6150e313095eb555696350aabe490efc9c
Ken Bonny <bonny.ken+github@gmail.com>
2021-12-03 19:09:04 +0100

    first day first problem

---
+++ AdventOfCode_2021/AdventOfCode_2021/Shared.fs
@@ -0,0 +1,17 @@
+﻿module AdventOfCode_2021.Shared
+
+open System
+open Xunit.Abstractions
+
+type OutputWriter(output: ITestOutputHelper) =
+    let agent =
+        MailboxProcessor.Start
+            (fun inbox ->
+                async {
+                    while true do
+                        let! message = inbox.Receive ()
+                        let now = DateTime.Now.ToString("HH:mm:ss:fff")
+                        output.WriteLine $"{now} -> {message}"
+                })
+
+    member _.writeToOutput text = agent.Post text
